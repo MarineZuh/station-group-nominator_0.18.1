98b6d93edb5d5523048382536fc098120eb0e209
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2021-04-24 20:47:42 -0300

    index starting number updata

--- control.lua
+++ control.lua
@@ -1,6 +1,7 @@
 local regexes = {} -- strings that will be replaced for numeric values | must be bettween { }
 local base_name = "" -- base name informed by user
 local entities = {} -- table of all entities on the selected area
+local starting_index = {} -- int of the starting index of every string regex; this is informed in the gui
 
 local gui_frame -- reference to the gui
 
@@ -34,7 +35,7 @@ local function get_new_station_name()
     return new_station_name
 end
 
-local function open_gui(event)
+local function open_gui_for_name(event)
     if event.item ~= "station-group-nominator" then return end
 
     local player = game.get_player(event.player_index)
@@ -56,37 +57,22 @@ local function open_gui(event)
                                            {
             type = "textfield",
             name = "gui_station_name_textfield"
-
         })
     local gui_ok_button = gui_frame.add({
         type = "button",
-        name = "gui-ok-button",
+        name = "gui-ok-name-button",
         caption = "OK"
     })
-    local gui_cancel_button = gui_frame.add({
-        type = "button",
-        name = "gui-cancel-button",
-        caption = "Cancel"
-    })
+    local gui_cancel_button = gui_frame.add(
+                                  {
+            type = "button",
+            name = "gui-cancel-button",
+            caption = "Cancel"
+        })
 
 end
 
-local function set_stations_name(event)
-    if event.element.name == "gui-cancel-button" then 
-        gui_frame.destroy()
-        return
-    end
-    if event.element.name ~= "gui-ok-button" then return end
-
-    for _, element in pairs(gui_frame.children) do
-        if element.name == "gui_station_name_textfield" then
-            base_name = element.text
-        end
-    end
-    gui_frame.destroy()
-
-    if (string.len(base_name) == 0) then return end
-
+local function set_new_stations_name()
     setup_regexes()
     for k, entity in pairs(entities) do
         if entity.type == "train-stop" and entity.supports_backer_name() then
@@ -97,5 +83,56 @@ local function set_stations_name(event)
     entities = {} -- reset entities
 end
 
-script.on_event(defines.events.on_player_selected_area, open_gui)
-script.on_event(defines.events.on_gui_click, set_stations_name)
+local function open_gui_regex_index(event)
+
+    local player = game.get_player(event.player_index)
+    if (player.gui.center["gui-frame"] ~= nil) then
+        player.gui.center["gui-frame"].destroy()
+    end
+
+    gui_frame = player.gui.center.add({
+        type = "frame",
+        direction = "horizontal",
+        name = "gui-frame",
+        caption = "Confirm starting numbers"
+    })
+
+    gui_frame.add({
+        type = "button",
+        name = "gui-ok-index-button",
+        caption = "OK"
+    })
+    gui_frame.add({
+        type = "button",
+        name = "gui-cancel-button",
+        caption = "Cancel"
+    })
+
+end
+
+local function check_gui_action(event)
+    if event.element.name == "gui-cancel-button" then
+        gui_frame.destroy()
+        return
+    end
+    if event.element.name == "gui-ok-name-button" then
+        -- exit if no name was provided
+        if (string.len(base_name) == 0) then return end
+        -- get text from textfield
+        for _, element in pairs(gui_frame.children) do
+            if element.name == "gui_station_name_textfield" then
+                base_name = element.text
+            end
+        end
+        -- close the gui 
+        gui_frame.destroy()
+        -- init next gui for index
+        open_gui_regex_index(event)
+    end
+    if event.element.name == "gui-ok-index-button" then
+        set_new_stations_name()
+    end
+end
+
+script.on_event(defines.events.on_player_selected_area, open_gui_for_name)
+script.on_event(defines.events.on_gui_click, check_gui_action)
